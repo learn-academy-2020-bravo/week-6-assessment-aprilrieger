d1abd00ba9b6a6ac36327d1c03ee9d194065ef9e
April Rieger <63515648+aprilrieger@users.noreply.github.com>
2020-06-05 15:51:00 -0700

    Completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -1,4 +1,4 @@
-// ASSESSMENT 6: Coding Practical Questions
+// April Rieger ------ ASSESSMENT 6: Coding Practical Questions
 
 
 // --------------------1) Create a function that takes in an array of objects and returns a sentence about each person with their names capitalized.
@@ -8,11 +8,21 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
-// Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
+// Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee.
 
-
-
-
+//create a function that takes an array of Objects as an arguement
+const loveObjects = (array) => {
+	//create a local variable to map through the value of the objects in the array
+  let firstLetterCapped = array.map(value => {
+  	//split the individual names into an array of substrings
+    let individualName = value.name.split(" ")
+    //return the varibale when we have mapped through the value of the individual name and capitalize the value at substring 1 which is the first character of each individual name, join it back together and concat the string interpolation to the end
+    return individualName.map(value => value.charAt(0).toUpperCase() + value.substring(1)).join(" ") + ` is a ${value.occupation}.`
+  })
+  //return the variable with .join since they are all in an individual array and the expected outcome is a single sentence.
+  return firstLetterCapped.join(" ")
+}
+console.log(loveObjects(people))
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the remainder of the numbers when divided by 3.
 
@@ -21,12 +31,29 @@ var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
 var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
-
-
-
+//create a function that takes in an array of mixed data
+const funWithArraysNot = (array) => {
+	//sort the data to exclude anything but numbers into a new array
+	let onlyNums = array.filter(value => typeof(value) === "number")
+	//return the varibale when we mapped through the value
+	return onlyNums.map(value => { 
+		//return the valu%3 into a new array
+		return value%3
+	})
+}
+console.log(funWithArraysNot(testingArray1))
+console.log(funWithArraysNot(testingArray2))
 
 // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+const moreFunWithArraysNot = (array1, array2) => {
+  let newArray = array1.concat(array2)
+  return newArray.filter((value, index) => {
+    return newArray.indexOf(value) === index
+  })
+}
+console.log(moreFunWithArraysNot(testingArray3, testingArray4))
--- interview-questions.md
+++ interview-questions.md
@@ -1,46 +1,41 @@
-# ASSESSMENT 6: Interview Practice Questions
+# April Rieger ------ ASSESSMENT 6: Interview Practice Questions
 Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn.
 
 1. In a model called Animal that has_many Sightings, what is the name of the foreign key? Would the foreign key be part of the Animal model or the Sightings model?
 
-  Your answer:
+  Your answer: The foreign key would be called animal_id. The foreign key will reside in Sightings.
 
-  Researched answer:
-
-
+  Researched answer: https://stackoverflow.com/questions/8510596/connecting-two-rails-models
 
 2. Which routes must always be passed params and why?
+ 
+  Your answer: Ummm... great question.... let me research and get back to you.
 
-  Your answer:
-
-  Researched answer:
-
-
+  Researched answer: Okay after research I was able to figure out that the routes that should always be passed are: Show, Update, and Delete. The reason is due to the face that we need to pass id through params.
 
 3. Write a rails route for a controller called "main" and a page called "game" that takes in a parameter called "guess".
 
-  Your answer:
-
-
+  Your answer: /game/:guess => game#main
 
 4. Name three rails generator commands. What is created by each?
 
-  Your answer:
+  Your answer: 
+  rails g migration
+  rails g model name:string age:integer
+  rails g resource name:string age:string
 
-  Researched answer:
-
-
+  Researched answer: Here are some more rails generator commands: https://guides.rubyonrails.org/command_line.html#rails-generate
 
 5. Consider the Rails routes below. Describe the responsibility of each route.
 
-/users        method="GET"
+/users        method="GET" I believe this will return an error becuase it is plural we need to define the id or remove the s at the end and get all the users.
 
-/users/1      method="GET"
+/users/1      method="GET" -- users#index - displays a list of all users
 
-/users/new    method="GET"
+/users/new    method="GET"  -- users#new - returns an HTML form creating a new user
 
-/users/       method="POST"
+/users/       method="POST"  -- users#create - creates a new user
 
-/users/1      method="PUT"
+/users/1      method="PUT"  -- users#update - updates a specific user with the id of 1 in this instance.
 
-/users/1      method="DELETE"
+/users/1      method="DELETE"  -- users#destroy - deletes a specific user with the id of 1 in this instance.
